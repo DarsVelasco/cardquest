6d74df9dca8b174c523fba1fcd49277e575e5526
DarsVelasco <darwin.velasco1210@gmail.com>
2023-12-12 21:26:59 +0800

    000005

--- projectsite/cardquest/forms.py
+++ projectsite/cardquest/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Trainer, Collection, PokemonCard
+
 class TrainerForm(ModelForm):
     birthdate = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     class Meta:
--- projectsite/cardquest/urls.py
+++ projectsite/cardquest/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from cardquest.views import HomePageView, TrainerList, TrainerCreateView, TrainerUpdateView, TrainerDeleteView
 from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('trainer', views.TrainerList.as_view(), name='trainer-list'),
     path('pokemon_card', views.PokemonCardList.as_view(), name='pokemon-card'),
     path('collection', views.CollectionList.as_view(), name='collection'),
-    path('trainer_list/add', views.TrainerCreateView.as_view(), name='trainer-add'),
-    
+    path('trainer_list', TrainerList.as_view(), name='trainer-list'),
+    path('trainer_list/add', TrainerCreateView.as_view(), name='trainer-add'),
+    path('trainer_list/<pk>', TrainerUpdateView.as_view(), name='trainer-update'),
+    path('trainer_list/<pk>/delete', TrainerDeleteView.as_view(), name='trainer-delete'),
 ]
