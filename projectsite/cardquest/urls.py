becfa580265f6c0c507533e79dd39704d02c77ca
DarsVelasco <darwin.velasco1210@gmail.com>
2023-12-14 21:40:36 +0800

    mag lagay ng cards pics

--- projectsite/cardquest/urls.py
+++ projectsite/cardquest/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from cardquest.views import HomePageView, TrainerList, TrainerCreateView, TrainerUpdateView, TrainerDeleteView,PokemonCardListView
 from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('pokemon_card', views.PokemonCardList.as_view(), name='pokemon-card'),
+    path('pokemon_card', views.PokemonCardListView.as_view(), name='pokemon-card'),
     path('collection', views.CollectionList.as_view(), name='collection'),
-    path('trainer_list', TrainerList.as_view(), name='trainer-list'),
-    path('trainer_list/add', TrainerCreateView.as_view(), name='trainer-add'),
-    path('trainer_list/<pk>', TrainerUpdateView.as_view(), name='trainer-update'),
-    path('trainer_list/<pk>/delete', TrainerDeleteView.as_view(), name='trainer-delete'),
-    path('pokemoncard_list', PokemonCardListView.as_view(), name='pokemoncard-list'),
+    path('trainer_list', views.TrainerList.as_view(), name='trainer-list'),
+    path('trainer_list/add', views.TrainerCreateView.as_view(), name='trainer-add'),
+    path('trainer_list/<pk>', views.TrainerUpdateView.as_view(), name='trainer-update'),
+    path('trainer_list/<pk>/delete', views.TrainerDeleteView.as_view(), name='trainer-delete')
 ]
