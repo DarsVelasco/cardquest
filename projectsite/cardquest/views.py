cfbf5fb5549b1a046e3548b2932057b2990823f6
DarsVelasco <darwin.velasco1210@gmail.com>
2023-12-15 11:36:00 +0800

    bago eto

--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from cardquest.models import Trainer, PokemonCard, Collection
-from cardquest.forms import TrainerForm
+from cardquest.forms import TrainerForm, CollectionForm, PokemonCardForm
 import json
 
 from django.urls import reverse_lazy
@@ -22,29 +22,8 @@ class TrainerList(ListView):
     model = Trainer
     context_object_name = 'trainer'
     template_name = 'trainer.html'
-    paginate_by = 9
-
-class PokemonCardListView(ListView):
-    model = PokemonCard
-    context_object_name = 'pokemoncard'
-    template_name = "pokemon-card.html"
-    json_file_path = 'cardquest/projectsite/data/pokemon_data.json'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        pokemon_data = self.get_pokemon_data()
-        context['pokemon_data'] = pokemon_data
-        return context
-    def get_pokemon_data(self):
-        with open(self.json_file_path, 'r') as file:
-            data = json.load(file)
-            return data.get('pokemons', [])
-
-class CollectionList(ListView):
-    model = Collection
-    context_object_name = 'collection'
-    template_name = 'collection.html'
-    paginate_by = 10
-
+    paginate_by = 7
+    
 class TrainerCreateView(CreateView):
     model = Trainer
     form_class = TrainerForm
@@ -61,5 +40,65 @@ class TrainerDeleteView(DeleteView):
     model = Trainer
     template_name = 'trainer_del.html'
     success_url = reverse_lazy('trainer-list')
+    
+    
+
+class PokemonCardListView(ListView):
+    model = PokemonCard
+    context_object_name = 'pokemoncard'
+    template_name = "pokemon-card.html"
+    paginate_by = 3
+    json_file_path = 'data/pokemon_data.json'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pokemon_data = self.get_pokemon_data()
+        context['pokemon_data'] = pokemon_data
+        return context
+    def get_pokemon_data(self):
+        with open(self.json_file_path, 'r') as file:
+            data = json.load(file)
+            return data.get('pokemons', [])
+        
+class PokemonCardCreateView(CreateView):
+    model = PokemonCard
+    form_class = PokemonCardForm
+    template_name = "pokemon-card_add.html"
+    success_url = reverse_lazy('pokemon-card')
+    
+    
+class PokemonCardUpdateView(UpdateView):
+    model = PokemonCard
+    form_class = PokemonCardForm
+    template_name = "pokemon-card_edit.html"
+    success_url = reverse_lazy('pokemon-card')
+
+class PokemonCardDeleteView(DeleteView):
+    model = PokemonCard
+    context_object_name = 'pokemoncard'
+    template_name = "pokemon-card_del.html"
+    success_url = reverse_lazy('pokemon-card')
 
 
+
+class CollectionList(ListView):
+    model = Collection
+    context_object_name = 'collection'
+    template_name = 'collection.html'
+    paginate_by = 7
+    
+class CollectionCreateView(CreateView):
+    model = Collection
+    form_class = CollectionForm
+    template_name = 'collection_add.html'
+    success_url = reverse_lazy('collection')
+    
+class CollectionUpdateView(UpdateView):
+    model = Collection
+    form_class = CollectionForm
+    template_name = 'collection_edit.html'
+    success_url = reverse_lazy('collection')
+    
+class CollectionDeleteView(CreateView):
+    model = Collection
+    template_name = 'collection_del.html'
+    success_url = reverse_lazy('collection')
